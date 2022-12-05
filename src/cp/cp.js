50f97cd505acb9561fc99ff2a7c34ef27c60d2af
superpuper777 <superfevrik@gmail.com>
2022-12-05 22:21:58 +0300

    feat: add cp and wt subtasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fork } from 'node:child_process';
+
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { stdin, stdout } from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileName = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(fileName, args.split(' '));
 };
 
-spawnChildProcess();
+spawnChildProcess('--arg1 --arg2');
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileName = join(__dirname, 'worker.js');
+const CPUcores = cpus().length;
+
+const worker = new Worker(fileName, {
+  workerData: 8,
+});
+
+worker.on('message', (msg) => {
+  console.log('Fibonacci result', msg);
+});
+
+const createWorkers = (cores) => {
+  const promises = [];
+  let num = 10;
+  for (let i = 0; i < cores; i++) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(fileName, {
+          workerData: num++,
+        });
+        worker.on('message', (data) => resolve({ status: 'resolved', data }));
+        worker.on('error', () => resolve({ status: 'error', data: null }));
+        worker.on('exit', (code) => {
+          if (code !== 0) {
+            resolve({ status: 'error', data: null });
+          }
+        });
+      })
+    );
+  }
+  return promises;
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  Promise.allSettled(createWorkers(CPUcores)).then((results) =>
+    results.forEach((result) => console.log(result.value))
+  );
 };
 
 await performCalculations();
